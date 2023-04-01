e83d685acea6c10f9c2f973aa29d64a7141231ee
“Iranga <im@Irangas-MacBook-Pro.local>
2023-04-01 12:13:40 +1100

    Initial commit

---
+++ PaymentService/src/test/java/com/imm/microservice/saga/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imm.microservice.saga.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ orderservice/src/test/java/com/imm/microservice/saga/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imm.microservice.saga.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
