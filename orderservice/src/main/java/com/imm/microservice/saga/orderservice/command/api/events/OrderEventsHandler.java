858bff7474b25f82e574fde45fe50ac240a26aaf
“Iranga <im@Irangas-MacBook-Pro.local>
2023-04-04 10:57:50 +1000

    Add commands and query for create an order

---
+++ orderservice/src/main/java/com/imm/microservice/saga/orderservice/command/api/events/OrderEventsHandler.java
@@ -0,0 +1,45 @@
+package com.imm.microservice.saga.orderservice.command.api.events;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import com.imm.microservice.saga.orderservice.command.api.data.Order;
+import com.imm.microservice.saga.orderservice.command.api.data.OrderRepository;
+
+@Component
+public class OrderEventsHandler {
+
+    private OrderRepository orderRepository;
+
+    public OrderEventsHandler(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @EventHandler
+    public void on(OrderCreatedEvent event) {
+        Order order = new Order();
+        BeanUtils.copyProperties(event,order);
+        orderRepository.save(order);
+    }
+
+    @EventHandler
+    public void on(OrderCompletedEvent event) {
+        Order order
+                = orderRepository.findById(event.getOrderId()).get();
+
+        order.setOrderStatus(event.getOrderStatus());
+
+        orderRepository.save(order);
+    }
+
+    @EventHandler
+    public void on(OrderCancelledEvent event) {
+        Order order
+                = orderRepository.findById(event.getOrderId()).get();
+
+        order.setOrderStatus(event.getOrderStatus());
+
+        orderRepository.save(order);
+    }
+}
