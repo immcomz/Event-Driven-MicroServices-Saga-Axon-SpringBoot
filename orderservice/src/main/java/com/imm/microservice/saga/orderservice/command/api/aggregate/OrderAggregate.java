bd211bd22775c99eb2d19d70dcfd94373cbf2c95
“Iranga <im@Irangas-MacBook-Pro.local>
2023-04-04 10:58:53 +1000

    create Axon Saga to aggregrate with command and queries for creating order

---
+++ orderservice/src/main/java/com/imm/microservice/saga/orderservice/command/api/aggregate/OrderAggregate.java
@@ -0,0 +1,81 @@
+package com.imm.microservice.saga.orderservice.command.api.aggregate;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+import com.imm.microservice.saga.orderservice.command.api.command.CreateOrderCommand;
+import com.imm.microservice.saga.orderservice.command.api.events.OrderCreatedEvent;
+
+@Aggregate
+public class OrderAggregate {
+
+    @AggregateIdentifier
+    private String orderId;
+    private String productId;
+    private String userId;
+    private String addressId;
+    private Integer quantity;
+    private String orderStatus;
+
+    public OrderAggregate() {
+    }
+
+    @CommandHandler
+    public OrderAggregate(CreateOrderCommand createOrderCommand) {
+        //Aggregate will Create Events related to the created commands
+        OrderCreatedEvent orderCreatedEvent
+                = new OrderCreatedEvent();
+        //copy all thr properties from command and event        
+        BeanUtils.copyProperties(createOrderCommand,
+                orderCreatedEvent);
+        // apply properties to playload        
+        AggregateLifecycle.apply(orderCreatedEvent);
+    }
+
+    //update event details to aggregate
+    @EventSourcingHandler
+    public void on(OrderCreatedEvent event) {
+        this.orderStatus = event.getOrderStatus();
+        this.userId = event.getUserId();
+        this.orderId = event.getOrderId();
+        this.quantity = event.getQuantity();
+        this.productId = event.getProductId();
+        this.addressId = event.getAddressId();
+    }
+
+    @CommandHandler
+    public void handle(CompleteOrderCommand completeOrderCommand) {
+        //Validate The Command
+        // Publish Order Completed Event
+        OrderCompletedEvent orderCompletedEvent
+                = OrderCompletedEvent.builder()
+                .orderStatus(completeOrderCommand.getOrderStatus())
+                .orderId(completeOrderCommand.getOrderId())
+                .build();
+        AggregateLifecycle.apply(orderCompletedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(OrderCompletedEvent event) {
+        this.orderStatus = event.getOrderStatus();
+    }
+
+    @CommandHandler
+    public void handle(CancelOrderCommand cancelOrderCommand) {
+        OrderCancelledEvent orderCancelledEvent
+                = new OrderCancelledEvent();
+        BeanUtils.copyProperties(cancelOrderCommand,
+                orderCancelledEvent);
+
+        AggregateLifecycle.apply(orderCancelledEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(OrderCancelledEvent event) {
+        this.orderStatus = event.getOrderStatus();
+    }
+}
